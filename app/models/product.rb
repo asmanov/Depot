fb5003e88d6f5380a7411583edc765a3a6ecaf0d
asmanov.a <asmanov.a@gmail.com>
2017-03-26 11:54:25 +0300

    Depot with validates and test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+
+validates :title, :description, :image_url, presence: true #проверяет содержание в полях название,
+#описание и ссылка на картинку
+validates :price, numericality: {greater_than_or_to: 0.01}#проверяет чтобы введеная цена была 
+#больше 0,01
+validates :title, uniqueness: true #проверяет что бы каждое название было уникально
+
+validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)\Z}i,
+message: 'URL must be in format GIF, JPG, PNG.'	
+}# проверяет правильность расширения картинки
+
 end
